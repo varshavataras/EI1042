f0e3c9149a0f962dc4ccada0e4036af22b825a11
varshavataras <35966376+varshavataras@users.noreply.github.com>
2019-09-13 17:49:27 +0200

    Add files via upload

---
+++ P1/partials/registerForm.php
@@ -0,0 +1,24 @@
+<main>
+	<h1>GestiÓn de Usuarios </h1>
+	<form class="fom_usuario" action="?action=registrar" method="POST">
+
+		<legend>Datos básicos</legend>
+		<label for="nombre">Nombre</label>
+		<br/>
+		<input type="text" name="userName" class="item_requerid" size="20" maxlength="25" value="<?php print $userName ?>"
+		 placeholder="Miguel Cervantes" />
+		<br/>
+		<label for="email">Email</label>
+		<br/>
+		<input type="text" name="email" class="item_requerid" size="20" maxlength="25" value="<?php print $email ?>"
+		 placeholder="kiko@ic.es" />
+		<br/>
+		<label for="passwd">Clave</label>
+		<br/>
+		<input type="password" name="passwd" class="item_requerid" size="8" maxlength="25" value="<?php print $passwd ?>"
+		/>
+		<br/>
+		<input type="submit" value="Enviar">
+		<input type="reset" value="Deshacer">
+	</form>
+</main>
