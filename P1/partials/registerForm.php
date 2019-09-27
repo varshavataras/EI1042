debfafe1c945a8ef91cc5ebd785f304999d3ae98
Ivan <al362851@uji.es>
2019-09-27 19:24:59 +0200

    Formulario insercion usuario

--- P1/partials/registerForm.php
+++ P1/partials/registerForm.php
@@ -1,3 +1,10 @@
+<!--/**
+*@title: Formulario de insercion de usuarios
+* @author Ivan <al362851@uji.es> * @copyright 2019 Ivan
+* @license CC-BY-NC-SA
+*/
+-->
+
 <main>
 	<h1>GestiÓn de Usuarios </h1>
 	<form class="fom_usuario" action="?action=registrar" method="POST">
@@ -6,17 +13,32 @@
 		<label for="nombre">Nombre</label>
 		<br/>
 		<input type="text" name="userName" class="item_requerid" size="20" maxlength="25" value="<?php print $userName ?>"
-		 placeholder="Miguel Cervantes" />
+		 placeholder="Miguel" />
+		<br/>
+		<label for="apellidos">Apellidos</label>
+		<br/>
+		<input type="text" name="userSurname" class="item_requerid" size="20" maxlength="40" value="<?php print $userSurname ?>"
+		 placeholder="Cervantes Garcia" />
 		<br/>
 		<label for="email">Email</label>
 		<br/>
 		<input type="text" name="email" class="item_requerid" size="20" maxlength="25" value="<?php print $email ?>"
 		 placeholder="kiko@ic.es" />
 		<br/>
+		<label for="dni">DNI</label>
+		<br/>
+		<input type="text" name="dni" class="item_requerid" size="20" maxlength="25" value="<?php print $dni ?>"
+		 placeholder="12345678A" />
+		<br/>
 		<label for="passwd">Clave</label>
 		<br/>
 		<input type="password" name="passwd" class="item_requerid" size="8" maxlength="25" value="<?php print $passwd ?>"
 		/>
+		<br/>
+		<label for="photo">Foto</label>
+		<br/>
+		<input type="text" name="photo" class="item_requerid" size="20" maxlength="40" value="<?php print $photo ?>"
+		 placeholder="foto.jpg" />
 		<br/>
 		<input type="submit" value="Enviar">
 		<input type="reset" value="Deshacer">
