84af101f1740f7688d406d11cca68a93e914ddc8
al362851 <48291485+al362851@users.noreply.github.com>
2019-10-25 17:30:13 +0200

    Add files via upload

---
+++ LAB/twentyseventeen-child-1/footer.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.2
+ */
+
+?>
+
+		<div ><!-- #content -->
+		<!--
+
+		<footer class="fixed_footer" role="contentinfo">
+		
+			
+			
+			<table>
+			<tr>
+			<td>
+			<b><p >EMPRESA</p></b>
+			</td>
+			<td>
+			<b><p>INFO DE INTERÉS</p></b>	
+			</td>
+			<td>
+			<b><p>AUTORES</p></b>	
+			</td>
+			</tr>
+			
+			<tr>
+			
+			<td>
+			<p>CIF: A-4256503-Z</p><p>Av. Vicent Sos Baynat, s/n </p><p>12071 Castelló de la Plana, Espanya</p>
+			</td>
+		
+			
+			<td>
+				<p>CopyRight</p>
+				<p><a href="/portal/acercade">Acerca De</a></p>
+				<p>Universitat Jaume I</p>
+			</td>
+			
+				
+			<td>
+			<p>Taras Varshava Aroca</p><p>Ivan Guimera Fontanet</p>
+			</td>
+				
+			
+			</tr>
+			</table>
+		</footer>
+//-->
+			
+			
+<footer class="footer-principal">
+	<div class="footer-left">
+		<h3>Uji<span>Motos</span></h3>
+		<p class="autores">
+		<a>Taras Varshava</a>
+		<a>Ivan Guimera</a>
+		</p>
+		<p class="footer-empresa">ujimotos &copy; 2019</p>
+	</div>
+ 
+	<div class="footer-center">
+ 		<div>
+			<p><span>Av. Vicent Sos Baynat, s/n</span> Universitat Jaume I
+			<br>
+			Castellon de la Plana, España</p>
+		</div>
+ 
+		<div>
+			<p>+34 123456</p>
+		</div>
+		<div>
+			<p><a href="mailto:ujimotos@ujimotos.com">ujimotos@ujimotos.com</a></p>
+		</div>
+ 
+	</div>
+ 
+	<div class="footer-right">
+ 		<p class="footer-sobre">
+			<span><a href="/portal/acercade">Sobre nosotros</a>
+	UjiMotos es una tienda online de venta de motos líder en la província de Castellón.</span>
+		</p>
+ 
+	</div>
+ 
+</footer>
+
+
+
+<!-- #colophon -->
+	</div><!-- .site-content-contain -->
+</div><!-- #page -->
+<?php wp_footer(); 
+
+//<footer class="footerMotos" role="contentinfo">
+//<footer id="colophon" class="site-footer" role="contentinfo">
+?>
+
+</body>
+</html>
