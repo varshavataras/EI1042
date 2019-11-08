10cf10e043bd047fa6cf9eb52e25e8a824a32a05
al362851 <48291485+al362851@users.noreply.github.com>
2019-11-08 17:22:07 +0100

    Update functions.php

--- LAB/P3/twentyseventeen-child-1/functions.php
+++ LAB/P3/twentyseventeen-child-1/functions.php
@@ -3,7 +3,7 @@ add_filter("the_content", "mfp_Fix_Text_Spacing");
 // Automatically correct double spaces from any post
 function mfp_Fix_Text_Spacing($the_Post)
 {
-$the_New_Post = str_replace("piruleta", " PIRULETA ", $the_Post);
+$the_New_Post = str_replace("motos", " MOTOS ", $the_Post);
 return $the_New_Post;
 }
 
