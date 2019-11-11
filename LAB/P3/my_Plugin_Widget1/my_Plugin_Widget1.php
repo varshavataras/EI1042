295a33b8db011bf5b1b5aacfb19a29d052074ac3
al362851 <48291485+al362851@users.noreply.github.com>
2019-11-11 15:19:11 +0100

    Update my_Plugin_Widget1.php

--- LAB/P3/my_Plugin_Widget1/my_Plugin_Widget1.php
+++ LAB/P3/my_Plugin_Widget1/my_Plugin_Widget1.php
@@ -32,11 +32,14 @@ public function __construct() {
 
 public function widget( $args, $instance ) {
 $title = apply_filters( 'widget_title', $instance['title'] );
+$direccion = apply_filters( 'widget_title', $instance['direccion'] );
 
 // los argumentos del antes y después del widget vienen definidos por el tema
 echo $args['before_widget'];
 if ( ! empty( $title ) )
 echo $args['before_title'] . $title . $args['after_title'];
+if ( ! empty( $direccion ) )
+echo $args['before_direccion'] . $direccion . $args['after_direccion'];
 
 // Aquí es donde debemos introducir el código que queremos que se ejecute
 echo 'Hola Mundo' ;
@@ -47,9 +50,11 @@ echo $args['after_widget'];
 public function form( $instance ) {
 if ( isset( $instance[ 'title' ] ) ) {
 $title = $instance[ 'title' ];
+$direccion = $instance[ 'direccion' ];
 }
 else {
 $title = __( 'Titulo', 'my_widget_domain' );
+$direccion = __( 'Dirección', 'my_widget_domain' );
 }
 // Formulario del backend
  ?>
@@ -57,12 +62,17 @@ $title = __( 'Titulo', 'my_widget_domain' );
 <label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Titulo:' ); ?></label> 
 <input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
 </p>
+<p>
+<label for="<?php echo $this->get_field_id( 'direccion' ); ?>"><?php _e( 'Dirección:' ); ?></label> 
+<input class="widefat" id="<?php echo $this->get_field_id( 'direccion' ); ?>" name="<?php echo $this->get_field_name( 'direccion' ); ?>" type="text" value="<?php echo esc_attr( $direccion ); ?>" />
+</p>
 <?php	
 }
 // Actualizamos el widget reemplazando las viejas instancias con las nuevas
 public function update( $new_instance, $old_instance ) {
 $instance = array();
 $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+$instance['direccion'] = ( ! empty( $new_instance['direccion'] ) ) ? strip_tags( $new_instance['direccion'] ) : '';
 return $instance;
 }
 } // La clase wp_widget termina aquí
