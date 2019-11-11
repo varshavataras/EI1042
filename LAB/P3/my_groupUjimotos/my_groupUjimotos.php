b80c25e047455e107b1155cf90cd2d2c04bebd45
al362851 <48291485+al362851@users.noreply.github.com>
2019-11-11 19:07:49 +0100

    Update my_groupUjimotos.php

--- LAB/P3/my_groupUjimotos/my_groupUjimotos.php
+++ LAB/P3/my_groupUjimotos/my_groupUjimotos.php
@@ -27,13 +27,13 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
 $table="A_GrupoCliente000";
 include(plugin_dir_path( __FILE__ ).'include/functions.php');
 
-register_activation_hook( __FILE__, 'MP_Ejecutar_crearT');
+register_activation_hook( __FILE__, 'UjiMotos_MP_Ejecutar_crearT');
 
 //add_action( 'plugins_loaded', 'Ejecutar_crearT' ); // esto se ejecuta siempre que se llama al plugin
 function MP_Ejecutar_crearT(){
     MP_CrearT("A_GrupoCliente000");
 }
 //add_action('admin_post_nopriv_my_datos', 'MP_my_datos'); //no autentificados
-add_action('admin_post_my_datos', "MP_my_datos"); 
+add_action('admin_post_my_datos', "UjiMotos_MP_my_datos"); 
 
 ?>
