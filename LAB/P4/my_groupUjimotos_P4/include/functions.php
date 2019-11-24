6903b0326c3ef115f682a22baabb5e34ad74858d
al362851 <48291485+al362851@users.noreply.github.com>
2019-11-24 11:19:31 +0100

    Add files via upload

---
+++ LAB/P4/my_groupUjimotos_P4/include/functions.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * * Descripción: Controlador principal
+ * *
+ * * Descripción extensa: Iremos añadiendo cosas complejas en PHP.
+ * *
+ * * @author  Lola <dllido@uji.es> 
+ * * @copyright 2018 Lola
+ * * @license http://www.fsf.org/licensing/licenses/gpl.txt GPL 2 or later
+ * * @version 2
+ * */
+
+
+//Estas 2 instrucciones me aseguran que el usuario accede a través del WP. Y no directamente
+if ( ! defined( 'WPINC' ) ) exit;
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+
+
+
+//Funcion instalación plugin. Crea tabla
+function UjiMotos_MP_CrearT($tabla){
+    
+    $MP_pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD); 
+    $query="CREATE TABLE IF NOT EXISTS $tabla (person_id INT(11) NOT NULL AUTO_INCREMENT, nombre VARCHAR(100),  email VARCHAR(100),  foto_file VARCHAR(100), clienteMail VARCHAR(100),  PRIMARY KEY(person_id))";
+    $consult = $MP_pdo->prepare($query);
+    $consult->execute (array());
+}
+
+
+function UjiMotos_MP_Register_Form($MP_user , $user_email)
+{//formulario registro amigos de $user_email
+    ?>
+    <h1>Gestión de Usuarios </h1>
+    <form class="fom_usuario" action="?action=my_datos_ujimotos&proceso_ujimotos=registrar_ujimotos" method="POST" enctype="multipart/form-data">
+        <label for="clienteMail">Tu correo</label>
+        <br/>
+        <input type="text" name="clienteMail"  size="20" maxlength="25" value="<?php print $user_email?>"
+        readonly />
+        <br/>
+        <legend>Datos básicos</legend>
+        <label for="nombre">Nombre</label>
+        <br/>
+        <input type="text" name="userName" class="item_requerid" size="20" maxlength="25" value="<?php print $MP_user["userName"] ?>"
+        placeholder="Nombre" />
+        <br/>
+        <label for="email">Email</label>
+        <br/>
+        <input type="text" name="email" class="item_requerid" size="20" maxlength="25" value="<?php print $MP_user["email"] ?>"
+        placeholder="ejemplo@correo.com" />
+        <br/>
+        <label for="foto_file">Foto</label>
+        <br/>
+        <input type="file" name="foto_file" value="<?php print $MP_user["foto_file"] ?>"
+         />
+        <p>
+        <input type="submit" value="Enviar">
+        <input type="reset" value="Deshacer">
+    </form>
+<?php
+}
+
+//CONTROLADOR
+//Esta función realizará distintas acciones en función del valor del parámetro
+//$_REQUEST['proceso'], o sea se activara al llamar a url semejantes a 
+//https://host/wp-admin/admin-post.php?action=my_datos&proceso=r 
+
+function UjiMotos_MP_my_datos()
+{ 
+    global $user_ID , $user_email,$table;
+    
+    $MP_pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD); 
+    wp_get_current_user();
+    if ('' == $user_ID) {
+                //no user logged in
+                exit;
+    }
+    
+    
+    
+    if (!(isset($_REQUEST['action'])) or !(isset($_REQUEST['proceso_ujimotos']))) { print("Opciones no correctas $user_email"); exit;}
+
+    get_header();
+    echo '<div class="wrap">';
+
+    switch ($_REQUEST['proceso_ujimotos']) {
+        case "registro_ujimotos":
+            $MP_user=null; //variable a rellenar cuando usamos modificar con este formulario
+            UjiMotos_MP_Register_Form($MP_user,$user_email);
+            break;
+        case "registrar_ujimotos":
+            if (count($_REQUEST) < 3) {
+                print ("No has rellenado el formulario correctamente");
+                return;
+            }
+            $fotoURL="";
+            $IMAGENES_USUARIOS = '../fotos/';
+            if(array_key_exists('foto_file', $_FILES) && $_POST['email']){
+                $fotoURL = $IMAGENES_USUARIOS.$_FILES['foto_file']['name'];
+                if (move_uploaded_file($_FILES['foto_file']['tmp_name'],$fotoURL)){
+                    echo "foto subida con éxito";
+                }
+            }
+            $query = "INSERT INTO $table (nombre, email,foto_file,clienteMail) VALUES (?,?,?,?)";         
+            $a=array($_REQUEST['userName'], $_REQUEST['email'], $fotoURL,$_REQUEST['clienteMail'] );
+            //$pdo1 = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASSWORD); 
+            $consult = $MP_pdo->prepare($query);
+            $a=$consult->execute($a);
+            if (1>$a) {echo "InCorrecto $query";}
+            else wp_redirect(admin_url( 'admin-post.php?action=my_datos_ujimotos&proceso_ujimotos=listar_ujimotos'));
+            break;
+        case "listar_ujimotos":
+			
+            //Listado amigos o de todos si se es administrador.
+            $a=array();
+            if (current_user_can('administrator')) {$query = "SELECT   *  FROM       $table ";}
+            else {$campo="clienteMail";
+                $query = "SELECT     * FROM  $table      WHERE $campo =?";
+                $a=array( $user_email);
+ 
+            } 
+            
+            $consult = $MP_pdo->prepare($query);
+            $a=$consult->execute($a);
+            $rows=$consult->fetchAll(PDO::FETCH_ASSOC);
+			
+            if (is_array($rows)) {/* Creamos un listado como una tabla HTML*/
+               	
+				echo "<div>","<table><tr>";
+				
+				
+                foreach ( array_keys($rows[0])as $key) {
+					
+                    echo "<th>",$key,"</th>";
+                }
+				
+				print "</tr>";
+                
+                foreach ($rows as $row) {
+                    print "<tr>";
+                    foreach ($row as $key => $val) {
+                        if ($key == 'foto_file'){
+                            echo "<td>",'<img src="'.$val.'" />',"</td>";
+                        }
+                        else{
+                            echo "<td>", $val, "</td>";
+                        }
+                    }
+                    print "</tr>";
+                }
+                print "</table></div>";
+            }
+            else{echo "No existen valores";}
+            break;
+        default:
+            print "Opción no correcta";
+        
+    }
+    echo "</div>";
+    // get_footer ademas del pie de página carga el toolbar de administración de wordpres si es un 
+    //usuario autentificado, por ello voy a borrar la acción cuando no es un administrador para que no aparezca.
+    if (!current_user_can('administrator')) {
+
+        // for the admin page
+        remove_action('admin_footer', 'wp_admin_bar_render', 1000);
+        // for the front-end
+        remove_action('wp_footer', 'wp_admin_bar_render', 1000);
+    }
+
+    get_footer();
+    }
+//add_action('admin_post_nopriv_my_datos', 'my_datos');
+//add_action('admin_post_my_datos', 'my_datos'); //no autentificados
+?>
