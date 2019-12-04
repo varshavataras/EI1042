1d8d50de9d9120f3b8ddc2dc11027e6ca20cbce7
varshavataras <35966376+varshavataras@users.noreply.github.com>
2019-12-04 19:17:19 +0100

    Update validar-form.php

--- LAB/P4/my_groupUjimotos_P4/include/validar-form.php
+++ LAB/P4/my_groupUjimotos_P4/include/validar-form.php
@@ -2,8 +2,12 @@
 	
 	if(isset($_POST['submit'])){
 		$imagen=$_POST['foto_file'];
+		$nombre=$_POST['userName'];
 		if (empty($imagen)){
-			echo "<p>AGREGA UNA FOTO PARA EL USUARIO";	
+			echo "<p>AGREGA UNA FOTO PARA EL USUARIO</p>";	
+		}
+		if (empty($nombre)){
+			echo "<p>AGREGA UNA FOTO PARA EL USUARIO</p>";	
 		}
 	}
 	
