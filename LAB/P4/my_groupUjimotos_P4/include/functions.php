0d751f4ac4cd634b3bfd4f6b7374f5a2b9b929bf
varshavataras <al361933@uji.es>
2019-12-05 02:21:38 +0100

    validacion_imagen

--- LAB/P4/my_groupUjimotos_P4/include/functions.php
+++ LAB/P4/my_groupUjimotos_P4/include/functions.php
@@ -129,45 +129,35 @@ function UjiMotos_MP_Register_Form($MP_user , $user_email)
 		}
 		ready();
 		
-		function comprueba_extension(formulario, archivo) {
-   
+   function comprueba_extension(formulario, archivo) {
+   extensiones_permitidas = new Array(".jpeg", ".jpg");
    mierror = "";
-   
-   var img = document.getElementById("img_foto");
-
-	alert(img.height);
-	//alert(img.naturalHeight);
-	//alert($("#img_foto").height());
-
    if (!archivo) {
       //Si no tengo archivo, es que no se ha seleccionado un archivo en el formulario
        mierror = "No has seleccionado ningún archivo";
-	   alert(mierror);
-	   
    }else{
       //recupero la extensión de este nombre de archivo
       extension = (archivo.substring(archivo.lastIndexOf("."))).toLowerCase();
       //alert (extension);
       //compruebo si la extensión está entre las permitidas
       permitida = false;
-      
-      if ("jpg" == extension || "jpeg" == extension) {
-		 permitida = true;
+      for (var i = 0; i < extensiones_permitidas.length; i++) {
+         if (extensiones_permitidas[i] == extension) {
+         permitida = true;
          break;
-       }
-		 
-      
-      if (!permitida) {
-         mierror = "Comprueba la extensión de los archivos a subir. \nSólo se pueden subir archivos con extensiones JPG O JPEG ";
+         }
       }
-	   else{
+      if (!permitida) {
+         mierror = "Comprueba la extensión de los archivos a subir. \nSólo se pueden subir archivos con extensiones: " + extensiones_permitidas.join();
+       }else{
           //submito!
+         
          formulario.submit();
          return 1;
        }
    }
    //si estoy aqui es que no se ha podido submitir
-   alert(mierror);
+   alert (mierror);
    return 0;
 } 
 		
